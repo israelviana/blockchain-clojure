2d671d6a4918b81e5556dfb8e0d8e3fba0378709
israelviana <rael232is@gmail.com>
2024-05-29 08:40:17 -0300

    finish blockchain project

--- src/handlers/blockchain.clj
+++ src/handlers/blockchain.clj
@@ -3,54 +3,58 @@
 
 (defonce blockchain (atom []))
 
-(defn get-blockchain []
-  @blockchain)
 
 (defn get-previous-block [chain]
   (last chain))
 
-(defn create-block [chain nonce previous-hash hash]
+(defn create-block [chain nonce previous-hash hash transactions]
   (let [index (inc (count chain))
         timestamp (str (java.util.Date.))]
     {:index index
      :timestamp timestamp
      :nonce nonce
-     :previous-hash previous-hash
-     :hash hash}))
+     :previousHash previous-hash
+     :hash hash
+     :transactions transactions}))
 
 (defn sha256 [data]
   (let [digest (.digest (MessageDigest/getInstance "SHA-256") (.getBytes data "UTF-8"))]
     (apply str (map (partial format "%02x") digest))))
 
-(defn proof-of-work [previous-nonce previous-hash transactions]
+(defn proof-of-work [previous-nonce previous-hash transactions index]
   (loop [new-nonce 1]
     (let [hash-operation (sha256 (str (- (Math/pow new-nonce 2)
-                                         (Math/pow previous-nonce 2)) transactions previous-hash))]
+                                         (Math/pow previous-nonce 2)) transactions previous-hash index))]
       (if (and (= (subs hash-operation 0 4) "0000"))
         [new-nonce hash-operation]
         (recur (inc new-nonce))))))
 
-
-(defn mine-block [transactions]
+(defn get-blockchain []
   (if (empty? @blockchain)
-    (let [previous-hash 0
+    (let [previousHash 0
           previous-nonce 0
-          [nonce hash-operation] (proof-of-work previous-nonce previous-hash transactions)
-          block (create-block @blockchain nonce previous-hash hash-operation)]
+          [nonce hash-operation] (proof-of-work previous-nonce previousHash [] 0)
+          block (create-block @blockchain nonce previousHash hash-operation [])]
       (swap! blockchain conj block)
       {:message "congratulations, genesis block is created"
        :index (:index block)
        :timestamp (:timestamp block)
        :nonce nonce
-       :previous-hash previous-hash})
-    (let [previous-block (get-previous-block @blockchain)
-          previous-nonce (:nonce previous-block)
-          previous-hash (:hash previous-block)
-          [nonce hash-operation] (proof-of-work previous-nonce previous-hash transactions)
-          block (create-block @blockchain nonce previous-hash hash-operation)]
-      (swap! blockchain conj block)
-      {:message "congratulations, you mined one block!"
-       :index (:index block)
-       :timestamp (:timestamp block)
-       :nonce nonce
-       :previous-hash previous-hash})))
+       :previousHash previousHash
+       :transactions []})
+    ())
+  @blockchain)
+
+(defn mine-block [transactions]
+  (let [previous-block (get-previous-block @blockchain)
+        previous-nonce (:nonce previous-block)
+        previousHash (:hash previous-block)
+        [nonce hash-operation] (proof-of-work previous-nonce previousHash transactions (:index previous-block))
+        block (create-block @blockchain nonce previousHash hash-operation transactions)]
+    (swap! blockchain conj block)
+    {:message "congratulations, you mined one block!"
+     :index (:index block)
+     :timestamp (:timestamp block)
+     :nonce nonce
+     :previousHash previousHash
+     :transactions (:transactions block)}))
